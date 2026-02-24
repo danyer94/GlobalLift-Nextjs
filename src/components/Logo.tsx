ab8d1de0ee3ce3ddfe01c097ba25d00f8a43788f
Danyer Acevedo <danyerag@gmail.com>
2026-02-24 15:23:22 -0500

    style: adjust logo size in Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,7 +15,7 @@ export function Logo({
         <Image
           src={withBasePath("/logo/logo-3D-removebg.png")}
           alt="Global Lift"
-          className="h-20 md:h-[6.5rem] w-auto -mt-2 -mb-2 scale-105"
+          className="h-[3.5rem] md:h-[4.5rem] w-auto"
           width={180}
           height={180}
           priority
